22b2484c84c75c87f817d73bb6c769d2426df794
Andrej Pedchenko <graduadu@gmail.com>
2022-03-20 15:03:48 +0200

    Improve to use Computed Property instead of each time concat part of video name.

--- Africa/View/VideoListItem.swift
+++ Africa/View/VideoListItem.swift
@@ -10,7 +10,7 @@ struct VideoListItem: View {
     var body: some View {
         HStack(spacing: 10) {
             ZStack {
-                Image("video-\(video.id)")
+                Image(video.thumbnail)
                     .resizable()
                     .scaledToFit()
                     .frame(height: 80)
