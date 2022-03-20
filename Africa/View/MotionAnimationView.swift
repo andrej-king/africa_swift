c0a51df3261967b9fef2dfeb2f9992c33143c487
Andrej Pedchenko <graduadu@gmail.com>
2022-03-20 22:10:03 +0200

    Created view for use as MOTION VIEW (gallery with random bubbles)

---
+++ Africa/View/MotionAnimationView.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+
+struct MotionAnimationView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var randomCircle = Int.random(in: 12...16)
+    
+    // MARK: - FUNCTIONS
+    
+    // 1. RANDOM COORDINATE
+    // 2. RANDOM SIZE
+    // 3. RANDOM SCALE
+    // 4. RANDOM SPEED
+    // 5. RANDOM DELAY
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                ForEach(0...randomCircle, id: \.self) { item in
+                    
+                    // This circle always will be in the center of any device
+                    Circle()
+                        .foregroundColor(.gray)
+                        .opacity(0.15)
+                        .frame(width: 256, height: 256, alignment: .center)
+                        .position(
+                            x: geometry.size.width / 2,
+                            y: geometry.size.height / 2
+                    )
+                } //: LOOP
+                
+                Text("Width: \(Int(geometry.size.width)) Height: \(Int(geometry.size.height))")
+            } //: ZSTACK
+        } //: GEOMETRY
+    }
+}
+
+// MARK: - PREVIEW
+
+struct MotionAnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        MotionAnimationView()
+    }
+}
