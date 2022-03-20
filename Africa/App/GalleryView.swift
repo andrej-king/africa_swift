a6c4c665ea372e7cb655ef930f9a0b4f58bd4ba3
Andrej Pedchenko <graduadu@gmail.com>
2022-03-20 15:08:03 +0200

    Added group comments

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -1,10 +1,16 @@
 import SwiftUI
 
 struct GalleryView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
     var body: some View {
         Text("Gallery")
     }
 }
+
+// MARK: - PREVIEW
 
 struct GalleryView_Previews: PreviewProvider {
     static var previews: some View {
--- Africa/App/MainView.swift
+++ Africa/App/MainView.swift
@@ -1,6 +1,10 @@
 import SwiftUI
 
 struct MainView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
     var body: some View {
         TabView {
             ContentView()
@@ -29,6 +33,8 @@ struct MainView: View {
         } //: TAB
     }
 }
+
+// MARK: - PREVIEW
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
