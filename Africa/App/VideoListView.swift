6b00b4a5161a819c91f9dadcdeb40f467aff867d
Andrej Pedchenko <graduadu@gmail.com>
2022-03-20 15:24:11 +0200

    Added VideoListItemView to the VideoListView.

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -3,10 +3,21 @@ import SwiftUI
 struct VideoListView: View {
     // MARK: - PROPERTIES
     
+    var videos: [Video] = Bundle.main.decode("videos.json")
+    
     // MARK: - BODY
     
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List {
+                ForEach(videos) { item in
+                    VideoListItemView(video: item)
+                        .padding(.vertical, 8)
+                } //: LOOP
+            } //: LIST
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+        } //: NAVIGATION
     }
 }
 
--- Africa/View/VideoListItemView.swift
+++ Africa/View/VideoListItemView.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-struct VideoListItem: View {
+struct VideoListItemView: View {
     // MARK: - PROPERTIES
     
     let video: Video
@@ -44,7 +44,7 @@ struct VideoListItem_Previews: PreviewProvider {
     static let videos: [Video] = Bundle.main.decode("videos.json")
     
     static var previews: some View {
-        VideoListItem(video: videos[0])
+        VideoListItemView(video: videos[0])
             .previewLayout(.sizeThatFits)
             .padding()
     }
