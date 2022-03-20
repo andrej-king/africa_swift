91ded9ee49653518d5eb796b1672323594f3ae94
Andrej Pedchenko <graduadu@gmail.com>
2022-03-21 00:58:51 +0200

    Added LazyVGrid to show list of animals image

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -3,11 +3,41 @@ import SwiftUI
 struct GalleryView: View {
     // MARK: - PROPERTIES
     
+    @State private var selectedAnimal: String = "lion"
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    // SIMPLE FRID DEFINITION
+    // let gridLayout: [GridItem] = [
+    //     GridItem(.flexible()),
+    //     GridItem(.flexible()),
+    //     GridItem(.flexible())
+    // ]
+    
+    // EFFICIENT GRID DEFINITION
+    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
     // MARK: - BODY
     
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            VStack(alignment: .center, spacing: 30) {
+                // MARK: - IMAGE
+                
+                // MARK: - GRID
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                    } //: LOOP
+                } //: GRID
+            } //: VSTACK
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         } //: SCROLL
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
