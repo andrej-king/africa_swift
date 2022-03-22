286e17053cf5f96634e73b2276403f7c603b9916
Andrej Pedchenko <graduadu@gmail.com>
2022-03-22 22:09:13 +0200

    Added grid view for animals

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -8,6 +8,8 @@ struct ContentView: View {
     
     @State private var isGridViewActive: Bool = false
     
+    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
+    
     // MARK: - BODY
     
     var body: some View {
@@ -26,7 +28,17 @@ struct ContentView: View {
                         } //: LOOP
                     } //: LIST
                 } else {
-                    Text("Grid view is active")
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                            ForEach(animals) { animal in
+                                NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                    AnimalGridItemView(animal: animal)
+                                } //: LINK
+                            } //: LOOP
+                        } //: GRID
+                        .padding(10)
+                        .animation(.easeIn, value: isGridViewActive)
+                    } //: SCROLL
                 } //: CONDITION
             } //: GROUP
             .listStyle(PlainListStyle())
