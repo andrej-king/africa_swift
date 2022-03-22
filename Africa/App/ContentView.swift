683d3e1445ca9c91276986b62d7c7a31b7d93889
Andrej Pedchenko <graduadu@gmail.com>
2022-03-22 21:52:26 +0200

    Added condition for grid view

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -12,17 +12,23 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            List {
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-            
-                ForEach(animals) { animal in
-                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
-                        AnimalListItemView(animal: animal)
-                    } //: LINK
-                }
-            } //: LIST
+            Group {
+                if !isGridViewActive {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        
+                        ForEach(animals) { animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                AnimalListItemView(animal: animal)
+                            } //: LINK
+                        } //: LOOP
+                    } //: LIST
+                } else {
+                    Text("Grid view is active")
+                } //: CONDITION
+            } //: GROUP
             .listStyle(PlainListStyle())
             .navigationBarTitle("Africa", displayMode: .large)
             .toolbar {
